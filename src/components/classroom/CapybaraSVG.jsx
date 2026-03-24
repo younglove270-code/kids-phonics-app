f703950007b93649e6664f8a7c0d78759516df10
younglove270-code <younglove270@gmail.com>
2026-03-25 01:01:39 +0800

    Create CapybaraSVG.jsx

---
+++ src/components/classroom/CapybaraSVG.jsx
@@ -0,0 +1,10 @@
+export default function CapybaraSVG({ className = "" }) {
+  return (
+    <img
+      src="https://media.base44.com/images/public/69c29b6ba4f1963e90202b13/b46f96e5f_generated_image.png"
+      alt="水豚君"
+      className={className}
+      draggable={false}
+    />
+  );
+}
